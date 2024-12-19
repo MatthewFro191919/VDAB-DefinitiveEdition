22d55532cbcb1b96dc1fc892b512ebe7c9dba8f1
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-19 15:29:43 -0500

    Update botplay kade engine.lua

--- example_mods/Vs Dave and Bambi 3.0 Shaggy Covers/scripts/botplay kade engine.lua
+++ example_mods/Vs Dave and Bambi 3.0 Shaggy Covers/scripts/botplay kade engine.lua
@@ -1,5 +1,6 @@
 function onUpdatePost(elapsed)
 	setProperty('botplayTxt.y', 565)
+	setTextFont('botplayTxt', 'comic');
 
     if getPropertyFromClass('ClientPrefs', 'downScroll') == true then
         setProperty('botplayTxt.y', 565)
